e45710c0ca98976ceae472443e8a67253f3348de
Dmanjav <A01753486@tec.mx>
2023-01-20 11:59:33 -0600

    cofla1

---
+++ 1. introduccion/codigo.js
@@ -0,0 +1,20 @@
+/*recipiente = "barbijo";
+
+alert(recipiente);*/
+
+/*
+Declaracion de variables
+let num1, num2, num3;
+
+num1 = 1;
+num2 = 10;
+num3 = 100;
+
+alert(num1);
+alert(num2);
+alert(num3);*/
+
+/**/
+let frase = prompt("dime tu nombre");
+
+alert("mucho gusto " + frase);
---
+++ Problemas de cofla 1/solucion1.js
@@ -0,0 +1,68 @@
+dineroCofla =  prompt("Cuanto dinero tienes Cofla?");
+
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+    alert("Puedes comprar el helado de awa");
+}
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+    alert("Puedes comprar el helado de crema");
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+    alert("Puedes comprar el helado de heladix");
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+    alert("Puedes comprar el helado de heladovich");
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+    alert("Puedes comprar el helado de helardo");
+}
+else if (dineroCofla >= 2.9) {
+    alert("Te puedes comprar el helado con confites o el bote de 1/4 de kg")
+} else {
+    alert("No te alcanza para nada conshesumadre")
+}
+
+dineroRoberto =  prompt("Cuanto dinero tienes Roberto?");
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
+    alert("Puedes comprar el helado de awa");
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+    alert("Puedes comprar el helado de crema");
+}
+else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+    alert("Puedes comprar el helado de heladix");
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+    alert("Puedes comprar el helado de heladovich");
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+    alert("Puedes comprar el helado de helardo");
+}
+else if (dineroRoberto >= 2.9) {
+    alert("Te puedes comprar el helado con confites o el bote de 1/4 de kg")
+} else {
+    alert("No te alcanza para nada conshesumadre")
+}
+
+dineroPedro =  prompt("Cuanto dinero tienes Pedro?");
+
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+    alert("Puedes comprar el helado de awa");
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+    alert("Puedes comprar el helado de crema");
+}
+else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+    alert("Puedes comprar el helado de heladix");
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+    alert("Puedes comprar el helado de heladovich");
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+    alert("Puedes comprar el helado de helardo");
+}
+else if (dineroPedro >= 2.9) {
+    alert("Te puedes comprar el helado con confites o el bote de 1/4 de kg")
+} else {
+    alert("No te alcanza para nada conshesumadre")
+}
